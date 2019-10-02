fc4bc1dd597a1330cc226e87bdf57a6d261c8430
bobbidigi <bobbidigi34@gmail.com>
2019-10-02 14:54:27 -0400

    add togglefunction

--- src/App.js
+++ src/App.js
@@ -1,13 +1,58 @@
 import React from 'react';
+import TodoList from './components/TodoComponents/TodoList';
+import './components/TodoComponents/Todo.css'
+
+let data = [
+  {
+    task: 'Organize Garage',
+    id: 1528817077286,
+    completed: false
+  },
+  {
+    task: 'Bake Cookies',
+    id: 1528817084358,
+    completed: false
+  }
+];
+
+
+// design `App` to be the parent component of your application.
+// this component is going to take care of state, and any change handlers you need to work with your state
+
 
 class App extends React.Component {
   // you will need a place to store your state in this component.
-  // design `App` to be the parent component of your application.
-  // this component is going to take care of state, and any change handlers you need to work with your state
+  constructor(){
+    super();
+    this.state = {
+      data
+    }
+  }
+
+  toggleItem = (event, itemId) => {
+    this.setState({
+      data: this.state.data.map(item => {
+        if(item.id === itemId){
+          return {
+            ...item,
+            completed: !item.completed
+          }
+        }else{
+          return item
+        }
+      })
+    })
+  }
+
+    
+
   render() {
     return (
       <div>
-        <h2>Welcome to your Todo App!</h2>
+        <div className="header">
+          <h1>To-Do-List</h1>
+        </div>
+        <TodoList toggleItem={this.toggleItem} data={this.state.data}/>
       </div>
     );
   }
--- src/components/TodoComponents/TodoList.js
+++ src/components/TodoComponents/TodoList.js
@@ -1,2 +1,19 @@
 // your components will all go in this `component` directory.
 // feel free to change this component.js into TodoList.js
+import React, { Component } from 'react'
+import Todo from './Todo';
+
+export default class TodoList extends Component {
+    render() {
+        console.log(this.props)
+        return (
+            <div className="shopping-list">
+                
+                {this.props.data.map((item) => <Todo
+                    item={item}
+                    key={item.id} 
+                    onClick={(e) => this.props.toggleItem(e, item.id)}/>)}
+            </div>
+        )
+    }
+}
